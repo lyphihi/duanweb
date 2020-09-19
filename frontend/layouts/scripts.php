64c7d30716df86db38513da6260604c8384ea385
STUDENT <thub1706651@student.ctu.edu.vn>
2020-09-19 19:24:46 +0700

    frontend  mail

---
+++ frontend/layouts/scripts.php
@@ -0,0 +1,3 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="/duanweb/assets/vendor/jquery/jquery.min.js"></script>
+<script src="/duanweb/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
